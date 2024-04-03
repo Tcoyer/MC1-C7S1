0f426c1845c1281b6d6a5eb327506b305c3c62f7
ferdinand7103 <ferdinand7103@gmail.com>
2024-04-03 11:00:55 +0700

    Fixed tenant page

--- MC1-C7S1/ApplicationPage/Tenant.swift
+++ MC1-C7S1/ApplicationPage/Tenant.swift
@@ -8,13 +8,8 @@
 import SwiftUI
 
 struct Tenant: View{
-    
-    
     var body: some View {
-        
-        // search
         @State var searchText = ""
-        
         
         HStack (alignment: .center) {
             VStack (alignment: .leading) {
@@ -27,6 +22,7 @@ struct Tenant: View{
                     .foregroundStyle(Color.white)
                     .bold()
             }.padding([.trailing], 90)
+                .padding([.top], 20)
             
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
             .background {
@@ -35,108 +31,35 @@ struct Tenant: View{
             }.padding([.top], 80)
         
         ZStack{
-            
             SearchBar()
                 .padding([.bottom], 625)
                 .frame(width: 380)
-            
-            VStack{
-                Image("Deal")
-                
-                
-                VStack{
-                    HStack{
-                        Text("Tenant List")
-                        Text("Tenant Map")
-                        
-                    }.padding([.bottom], 20)
-                    
-                    
-                    HStack{
-                        cardTenant()
-                        cardTenant()
-                        
+            VStack {
+                VStack(alignment: .leading){
+                    Text("Select the tenant")
+                        .font(.custom("SF Pro", size: 20))
+                        .bold()
+                    ScrollView{
+                        HStack{
+                            cardTenant(imageName: "Kasturi", name: "Kasturi", time: "7 - 12 min")
+                                .onTapGesture {
+                                    goTenant()
+                                }
+                            cardTenant(imageName: "Dapoer", name: "Dapoer Cowek", time: "5 - 10 min")
+                        }
+                        HStack{
+                            cardTenant(imageName: "Ahza", name: "Ahza Food", time: "7 - 12 min")
+                            cardTenant(imageName: "Mustafa", name: "Mustafa Minang", time: "7 - 12 min")
+                        }
                     }
-                    
-                    HStack{
-                        cardTenant()
-                        cardTenant()
-                        
-                    }
-                    //                        //
-                    //                        HStack{
-                    //                            VStack(alignment: .leading){
-                    //                                Image("Kasturi")
-                    //                                HStack{
-                    //                                    Text("Kasturi")
-                    //                                        .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
-                    //                                        .padding([.trailing], 35)
-                    //                                    Image(systemName: "star.fill")
-                    //                                        .foregroundColor(Color.yellow)
-                    //                                        .font(.footnote)
-                    //                                    Text("4.2 / 5")
-                    //                                        .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
-                    //                                        .padding([.leading], -5)
-                    //                                }
-                    //                                Text("7 - 12 min")
-                    //                                    .font(/*@START_MENU_TOKEN@*/.caption2/*@END_MENU_TOKEN@*/)
-                    //                                    .foregroundStyle(Color(red: 0.570, green: 0.570, blue: 0.570))
-                    //                            }.padding([.trailing], 10)
-                    //                            VStack(alignment: .leading){
-                    //                                Image("Dapoer")
-                    //                                HStack{
-                    //                                    Text("Ahza Snack")
-                    //                                        .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
-                    //
-                    //                                    Image(systemName: "star.fill")
-                    //                                        .foregroundColor(Color.yellow)
-                    //                                        .font(.footnote)
-                    //                                    Text("4.2 / 5")
-                    //                                        .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
-                    //                                        .padding([.leading], -5)
-                    //                                }
-                    //                                Text("5 - 10 min")
-                    //                                    .font(/*@START_MENU_TOKEN@*/.caption2/*@END_MENU_TOKEN@*/)
-                    //                                    .foregroundStyle(Color(red: 0.570, green: 0.570, blue: 0.570))
-                    //                            }
-                    //                        }
-                    //                        HStack{
-                    //                            VStack(alignment: .leading){
-                    //                                Image("Wong")
-                    //                                HStack{
-                    //                                    Text("Wong Jowo")
-                    //                                    Image(systemName: "star.fill")
-                    //                                        .foregroundColor(Color.yellow)
-                    //                                        .font(.footnote)
-                    //                                    Text("4.2 / 5")
-                    //                                        .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
-                    //                                        .padding([.leading], -5)
-                    //                                }
-                    //                                Text("7 - 12 min")
-                    //                                    .font(/*@START_MENU_TOKEN@*/.caption2/*@END_MENU_TOKEN@*/)
-                    //                                    .foregroundStyle(Color(red: 0.570, green: 0.570, blue: 0.570))
-                    //                            }.padding([.trailing], 10)
-                    //                            VStack(alignment: .leading){
-                    //                                Image("Lading")
-                    //                                HStack{
-                    //                                    Text("La Ding")
-                    //                                    Image(systemName: "star.fill")
-                    //                                        .foregroundColor(Color.yellow)
-                    //                                        .font(.footnote)
-                    //                                    Text("4.2 / 5")
-                    //                                        .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
-                    //                                        .padding([.leading], -5)
-                    //                                }
-                    //                                Text("5 - 10 min")
-                    //                                    .font(/*@START_MENU_TOKEN@*/.caption2/*@END_MENU_TOKEN@*/)
-                    //                                    .foregroundStyle(Color(red: 0.570, green: 0.570, blue: 0.570))
-                    //                            }
-                    //                        }
-                    //
                 }.padding([.top], 20)
-            }.padding([.top], 100)
+            }.padding([.top], 50)
         }.padding([.top], 50)
     }
+}
+
+func goTenant(){
+    Menu()
 }
 
 #Preview {
--- MC1-C7S1/Component/cardTenant.swift
+++ MC1-C7S1/Component/cardTenant.swift
@@ -8,25 +8,21 @@
 import SwiftUI
 
 struct cardTenant: View {
+    var imageName: String
+    var name: String
+    var time: String
     var body: some View {
         ZStack{
-            Rectangle().frame(width: 170, height: 200)
-                .foregroundColor(.white).shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            Rectangle().frame(width: 170, height: 210)
+                .foregroundColor(Color(hue: 0.0, saturation: 0.0, brightness: 1.0, opacity: 0.694)).shadow(radius: 10)
             VStack(alignment: .leading){
-                Image("Kasturi")
+                Image(imageName)
                 HStack{
-                    Text("Kasturi")
-                        .font(/*@START_MENU_TOKEN@*/.body/*@END_MENU_TOKEN@*/)
-                        .padding([.trailing], 35)
-                    Image(systemName: "star.fill")
-                        .foregroundColor(Color.yellow)
-                        .font(.footnote)
-                    Text("4.2 / 5")
-                        .font(/*@START_MENU_TOKEN@*/.footnote/*@END_MENU_TOKEN@*/)
-                        .padding([.leading], -5)
+                    Text("\(name)")
+                        .font(.custom("SF Pro", size: 18))
                 }
-                Text("7 - 12 min")
-                    .font(/*@START_MENU_TOKEN@*/.caption2/*@END_MENU_TOKEN@*/)
+                Text("\(time)")
+                    .font(.custom("SF Pro", size: 14))
                     .foregroundStyle(Color(red: 0.570, green: 0.570, blue: 0.570))
             }
         }
@@ -34,6 +30,6 @@ struct cardTenant: View {
     
 }
 
-#Preview {
-    cardTenant()
+#Preview{
+    cardTenant(imageName: "Mustafa", name: "Mustafa Minang", time: "3 - 10 min")
 }
